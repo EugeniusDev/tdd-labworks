a62e7b94df555713982512094da653a8fe5c2140
EugeniusDev <yevhenii.froliak@gmail.com>
2026-03-27 09:04:31 +0200

    Init commit

---
+++ src/test/java/edu/froliak/tddlabworks/TddLabworksApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.froliak.tddlabworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddLabworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
